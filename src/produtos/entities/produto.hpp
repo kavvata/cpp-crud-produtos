ff6d9408c3fd47a0e9bc57db4f51914d522c4e0b
kavvata <gabriel.kawata@gmail.com>
2025-09-13 14:06:32 -0300

    feat(produto): criado entidade para produto

---
+++ src/produtos/entities/produto.hpp
@@ -0,0 +1,19 @@
+#ifndef PRODUTO_HPP
+#define PRODUTO_HPP
+#include <string>
+#include "unidademedida.hpp"
+
+struct Produto {
+    static constexpr int SEM_CODIGO = -1;
+
+    int codigo;
+    std::string nome;
+    double preco;
+    int quantidade;
+    UnidadeMedida unidadeMedida;
+
+    Produto() : codigo{SEM_CODIGO}, nome{}, preco{0.0}, quantidade{0}, unidadeMedida{} {}
+    Produto(int c, const std::string& n, double p, int q, UnidadeMedida um)
+        : codigo{c}, nome{n}, preco{p}, quantidade{q}, unidadeMedida{um} {}
+};
+#endif  // !PRODUTO_HPP
